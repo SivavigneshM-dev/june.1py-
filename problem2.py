9919c208813ba3cf09e8c796877524f1a3fd8c82
Siva010q <suryaarts110@gmail.com>
2025-06-23 12:32:44 +0530

    pyton_sums

---
+++ problem2.py
@@ -0,0 +1,9 @@
+fruits_stock = {
+    "apple":10,
+    "banana":0,
+    "mango":5
+
+}
+
+def check_stock(fruit_name,fruit_stock)
+    fruits_stock_set = set (fruits_stock.lower() for check_stock in fruits_stock)
