e488a65cc78af0b66821857c6dfdc0f0c8d1f04b
Siva010q <suryaarts110@gmail.com>
2025-06-30 16:11:40 +0530

    commit

---
+++ looping.py
@@ -0,0 +1,71 @@
+# age = int(input())
+
+# if age >= 18 :
+#     print ("Eligible for voting")
+# else :
+#     print("Not Eligible for Voting ")
+
+# if else statements /\
+
+# _____________________
+
+#while loop
+
+# count  = 0 
+# while (count <= 3 ):
+#     count = count + 1
+#     print("Hello vicky brooo..")
+
+# _____________________
+
+#for loop syntax 
+
+# n = 5 
+# for i in range (0, n ):
+#     print(i)
+
+# _____________________
+
+#Nested loops Syntax 
+
+# from __future__ import print_function
+# for i in range (1,5):
+#     for j in range(i):
+#         print(i, end=' ')
+#     print() 
+
+# _____________________
+
+# break statement
+
+# for number in '9235279272':
+#     if number == '2' or '9' :
+#         break
+# print('current number :',number )
+
+# _____________________
+
+# functions
+# def  evenOdd(x: int) ->str:
+#     if (x / 2 == 0) :
+#         return "Even"
+#     else:
+#         return "Odd"
+# print(evenOdd(12))
+# print(evenOdd(9))
+
+# _____________________
+
+
+#Positional Arguments
+# def nameAge(name ,age):
+#     print("Hi ,I am ",name)
+#     print("My age is ", age)
+
+
+# print("case-1:")
+# nameAge("Siva", 21 )
+
+
+# print("\ncase-2:")
+# nameAge(21 ,"Siva")
--- problem2.py
+++ problem2.py
@@ -1,36 +1,16 @@
-# name = input("Enter Your Name")
-# print(name.upper())
-
-# from datetime import datetime
-
-# while True:
-#     dob_str = input("Enter your date of birth (YYYY-MM-DD): ")
-#     try:
-#         date_of_birth = datetime.strptime(dob_str, "%Y-%m-%d")
-#         print(f"Your date of birth is: {date_of_birth.strftime('%Y-%m-%d')}")
-#         break 
-#     except ValueError:
-#         print("Invalid date format. Please use YYYY-MM-DD.")
-
-# def assign_name_date_of_birth(name,date_of_birth):
-#     sum("%y","%d"):
 name = input("Enter Your Name")
-def generate_password(name, dob):
-    
+print(name.upper())
 
-    name = name.lower().replace(" ", "")
-    day, month, year = map(int, dob.split("-"))
+from datetime import datetime
 
-    password = name[:3].upper()
+while True:
+    dob_str = input("Enter your date of birth (YYYY-MM-DD): ")
+    try:
+        date_of_birth = datetime.strptime(dob_str, "%Y-%m-%d")
+        print(f"Your date of birth is: {date_of_birth.strftime('%Y-%m-%d')}")
+        break 
+    except ValueError:
+        print("Invalid date format. Please use YYYY-MM-DD.")
 
-    password += str(sum(int(digit) for digit in str(day) + str(month)))
-    
-    password += str(year % 100)[::-1]
-
-    vowels = "aeiou"
-    password += str(sum(1 for char in name if char in vowels))
-
-    return password
-
-print(generate_password)
-
+def assign_name_date_of_birth(name,date_of_birth):
+    sum("%y","%d")
