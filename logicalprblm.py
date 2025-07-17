23ca4136e5a47ec41b4a828ae79f7cb9a315836d
Siva010q <suryaarts110@gmail.com>
2025-07-17 20:00:21 +0530

    print added

--- logicalprblm.py
+++ logicalprblm.py
@@ -73,33 +73,34 @@ while i <= nubr:
    
 
 #print even numbers 
-print("Now printing even numbers !!!")
+print("Now printing even numbers by loop !!!")
 
 e = 2
 even = int(input("enter any even number  : "))
 while e <= even:
-    print(e)
+    print(e,end=",")
     e = e + 2
 
 
 #Odd numbers 
-print("Now printing odd numbers !!!")
+print("Now printing odd numbers  by loop !!!")
 
 o = 1
 odd = int(input("Enter any odd number  :"))
 while o<=odd :
-    print(o)
+    print(o,end=",")
     o += 2
 
 # Calculate product
 
 print("Product ")
+
 numbers = input("Enter two numbers separated by '/': ")
 
 num1, num2 = map(int, numbers.split('/'))
 
-
 product = num1 * num2
+
 print(product)
 
 
@@ -114,6 +115,7 @@ for i in range(1, rows + 1):
     print()
 
 #print list 
+print("print the list !!")
 tup = tuple(input("enter five numbers : "))
 
 li = list(tup)
@@ -121,7 +123,8 @@ li = list(tup)
 print(li)
 
 # Average
-avg = input("Enter three numbers 'with (,)  :  ")
+print("Average")
+avg = input("Enter three numbers 'with (,)':  ")
 numbr = [int(num.strip()) for num in avg.split(',')]
 
 total = sum(numbr)
@@ -129,7 +132,9 @@ average = total / len(numbr)
 
 print(average)
 
+
 #Assending order 
+print("Assending order")
 
 num = input("Enter the numbers: ")        
 digits = list(num)                       
@@ -139,6 +144,8 @@ print(sorted_num)
 
 
 #Descending order
+print("Descending order")
+
 no = input("Enter the numbers: ")        
 digit = list(no)                        
 digit.sort(reverse=True)          
@@ -146,6 +153,8 @@ sort_no = ''.join(digit)
 print(sort_no)
 
 #only odd numbers 
+
+print("Only odd numbers ")
 on= input("Enter multiple numbers separated by spaces: ").split()
 
 odd_number = []
@@ -158,6 +167,9 @@ print(odd_number)
 
 
 #only Even  numbers 
+
+print("Only Even  numbers ")
+
 en= input("Enter multiple numbers separated by spaces: ").split()
 
 eve_number = []
@@ -170,6 +182,7 @@ print(eve_number)
 
 
 #vowels only 
+print("Vowels Only ")
 vowel =  input("enter some text ")
 
 vowel_Text = []
@@ -182,20 +195,22 @@ for ch in vowel:
 print(vowel_Text)
 
 
-#vowels counts
+#vowels count !!!
+print("vowel text count")
 vowel =  input("enter some text ")
 
 vowel_Text = []
 check_Txt = ["a","e","i", "o", "u"]
 
-for ch in vowel:
-    if ch.lower() in check_Txt:
-        vowel_Text.append(ch)
+for cv in vowel:
+    if cv.lower() in check_Txt:
+        vowel_Text.append(cv)
 
 print(len(vowel_Text))
 
 
 #list comparision
+print("list comparision ")
 
 l1 = list(map(int,input("Enter some numbers with space: ").split()))
 l2 = list(map(int,input("Enter some numbers with space: ").split()))
@@ -205,6 +220,9 @@ else:
  print("The list is Not Equal")
 
 #To find positive or Negative 
+
+print("positive or negative number ")
+
 think = int(input("Enter positive or negative number  :"))
 if think > 0:
  print("Positive Number")
@@ -212,6 +230,8 @@ else :
     print("Negative Number ")
 
 #Array reverse
+print("Array reverse")
+
 avg = input("Enter three numbers separated by commas: ")
 numbr = [int(num.strip()) for num in avg.split(',')]
 
@@ -222,6 +242,7 @@ sort_Avg = ', '.join(str(num) for num in numbr)
 print(sort_Avg)
 
 #Largest number in a array 
+print("Biggest number in the array ")
 array1 = input("Enter numbers with space : ")
 arr = array1.split(' ')
 arr = [int(x.strip()) for x in arr]  
@@ -230,12 +251,23 @@ print(max(arr))
 
 
 #Smallest number in a array 
+
+print("Smallest number in the array ")
 array1 = input("Enter numbers with space : ")
 arr = array1.split(' ')
 arr = [int(x.strip()) for x in arr]  
 print(min(arr))
 
+# Reverse string 
 
-#Reverse string 
+print("Reversing the String ")
 
-string11 = input("") 
+string11 = input("Input some letters with space: ")
+string11 = string11.lower()  
+string2 = string11.split(' ')
+string2.reverse()  
+print(string2)
+
+
+
+
