aea928b5c6e489cd3a1ff64746091160925e8e8a
Siva010q <suryaarts110@gmail.com>
2025-10-07 15:01:12 +0530

    today's Loop section

--- oct07.py
+++ oct07.py
@@ -1,6 +1,5 @@
-"""
 
-# Today going to work with while 
+# Today going to work with while Loop ✌️
 
 print("1 to  10 ")
 
@@ -44,4 +43,125 @@ while num <= 20:
         print(num)
     num += 1
 
-"""
+
+# Multiplication table 5
+
+i = 1
+t = 5
+
+while i <= 10:
+    print(i,"X",t,"=",i*t)
+    i += 1
+
+# Level 2 :-   Input Based 
+# user  Input Sum  until 0 
+
+total = 0
+
+while True :
+    i = float(input("Numbers if '0 stop' "))
+    if i == 0:
+        break
+    total += i
+print(total)
+
+
+
+# Password checker 
+password = input("Enter the New password  :")
+retry = 0
+
+while True:
+    i = input("Enter the correct password !  :")
+    if i == password:
+        break
+    retry += 1
+print("ACCESS GARNTED ")
+
+# Positive Numbers only 
+
+i = 0 
+while True:
+    n = int(input("+ve Number   :"))
+    if n < 0 :
+        break
+    i += 1
+print("OOPs You Entered -ve Value ")
+
+# Guess the Number 
+
+k = 0 
+while True :
+    u = int(input("No  1-10  H or L '0 to stop '  :"))
+    if u > 5:
+        print("High")
+    elif u < 5:
+        print("Low")
+    elif u == 5 or u == 0:
+        break
+    else:
+        print("Number between 1 to 10 !")
+        
+    k += 1
+
+
+
+# Calculate the factorial 
+
+n = int(input("Factorial Calc: "))
+fact = 1
+i = n
+
+while i > 0:
+    fact *= i
+    i -= 1
+
+print(fact)
+
+
+# Level 3 :- String Manipulation 
+
+# Reverse String
+s = input("Text: ")
+text = ""
+i = len(s) - 1
+
+while i >= 0:
+    text += s[i]
+    i -= 1
+print(text)
+
+# Remove spaces
+text = input("Text: ")
+result = ""
+i = 0
+
+while i < len(text):
+    if text[i] != " ":
+        result += text[i]
+    i += 1
+
+print(result)
+
+
+# Conut Vowels 
+vowels = "aeiou"
+count = 0
+u = input("text")
+i = len(u)-1
+while i >= 0:
+    if u[i].lower()in vowels:
+        count += 1
+    i -= 1
+print(count)
+
+# Count digits in a string
+text = input("Number  :")
+count = 0
+i = 0
+while i < len(text):
+    if text[i].isdigit():
+        count += 1
+    i += 1
+
+print(count)
